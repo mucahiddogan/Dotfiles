cd808329b9beeb6f9de7331141ea626c0887e762
Mücahid Doğan <mucahid.dogan@ceng.ktu.edu.tr>
2017-11-29 03:22:33 +0300

    Update some_solutions.sh

--- some_solutions.sh
+++ some_solutions.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sudo mount -t ntfs-3g -o remove_hiberfile /dev/sda[xyz] /media/username/dontusewindows/
+sudo ntfsfix /dev/sda[xyz] 
+sudo mount -t ntfs-3g -o remove_hiberfile /dev/sda[xyz] /media/username/dontusewindows/ 
 sudo apt-get install vim
