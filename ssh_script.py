d03014d0d30e0e430c37ddbd0a00be2e5703fbe0
mucahiddogan <mucahid.dogan@ceng.ktu.edu.tr>
2020-08-17 13:44:20 +0300

    ssh brute script added

---
+++ ssh_script.py
@@ -0,0 +1,15 @@
+# nmap ve netdiscover dogru surec uretmediginde brute
+import os
+import subprocess
+from subprocess import call
+
+dizi = list()
+
+for i in range(0,256):
+    dizi.append(i)
+
+for i in range(0,256):
+    dizi[i] = str(dizi[i])
+    cmd = "ssh pi@ipaddrr." + dizi[i]
+   
+    os.system(cmd)
